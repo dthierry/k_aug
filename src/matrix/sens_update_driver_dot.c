683310b89213b4c77df0b340a13b9f80a336da6a
dthierry <dmolinat@andrew.cheme.cmu.edu>
2017-07-03 21:33:02 -0400

    dot_driver

--- src/matrix/sens_update_driver_dot.c
+++ src/matrix/sens_update_driver_dot.c
@@ -1,5 +1,5 @@
 
-#include "sens_update_driver.h"
+#include "sens_update_driver_dot.h"
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -8,16 +8,17 @@
  integer INCX, double precision BETA, double precision, dimension(*) Y,integer INCY
 */
 /* int n_rhs, fint rhs_len, real *rhsbksolv, real *dp, fint nvar, fint ncon, SufDesc **rhs_ptr */
-void sens_update_driver(int n_rhs, fint rhs_len, real *dsdp, real *dp, real *sstar){
-	char t = 'N';
+void sens_update_driver_dot(fint n_k, fint n_u, real *s_hat_t, real *npdp, real *u_star){
+	char t = 'T';
 
-	double ALPHA = -1.0;
+	double ALPHA = 1.0;
 	/*int LDA = 10;*/
 	int INCX = 1;
 	double BETA = 1.0;
 	int INCY = 1;
 
 
-	dgemv_(&t, &rhs_len, &n_rhs, &ALPHA, dsdp, &rhs_len, dp, &INCX, &BETA, sstar, &INCY);	
+	dgemv_(&t, &n_k, &n_u, &ALPHA, s_hat_t, &n_k, npdp, &INCX, &BETA, u_star, &INCY);
+	/*dgemv_(&t, &n_u, &n_k, &ALPHA, s_hat_t, &n_u, npdp, &INCX, &BETA, u_star, &INCY);	*/
 
 }
