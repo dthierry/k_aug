2cd909c9876dc3b885e2f16aebdf34b794f64015
dthierry <dmolinat@andrew.cheme.cmu.edu>
2018-04-18 21:53:36 -0400

    test for mumps driver

--- src/interfaces/mumps/mumps_driver.c
+++ src/interfaces/mumps/mumps_driver.c
@@ -30,13 +30,14 @@
 #include <stdlib.h>
 
 #include "mumps_driver.h"
+#include "mpi.h"
 #include "../../../thirdparty/mumps512/MUMPS_5.1.2/include/dmumps_c.h"
 
 /*int mumps_driver (fint *ia, fint *ja, real *a, fint n,
 	fint n_rhs, real *b, real *x, fint nvar, fint ncon, int no_inertia, int nza, double logmu0);
- int mumps_driver(fint *ia, fint *ja, real *a, fint n,
+ int mumps_driver(fint *ia, fint *ja, real *a, fi nt n,
 	fint n_rhs, real *b, real *x, fint nvar, fint ncon, int no_inertia, int nza, double logmu0){*/
-
+/*pardiso_driver(Kr_strt, Kcol, Kij, K_nrows, n_dof, rhs_baksolve, x_, n_var, n_con, no_inertia, nzK, logmu0, 1);*/
 int mumps_driver(fint *ia, fint *ja, real *a, fint n,
 	fint n_rhs, real *b, real *x, fint nvar, fint ncon, int no_inertia, int nza, double logmu0){
     /* Extra algorithmic steps:
@@ -46,6 +47,10 @@ int mumps_driver(fint *ia, fint *ja, real *a, fint n,
      * soft-regularization
      * */
 	DMUMPS_STRUC_C id;	/* datastructure */
+
+    int myid, ierr;
+    ierr = MPI_Init(NULL, NULL);
+    ierr = MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
 	id.sym = 2;	/* symmetric indefinite matrix */
 	id.comm_fortran = 2049;	/* i have no idea of what this is */
@@ -58,24 +63,36 @@ int mumps_driver(fint *ia, fint *ja, real *a, fint n,
 
 	id.job = -1;  /* initial job */
 
+    dmumps_c(&id);
+
+    if (myid == 0){
+        id.n = n;
+        id.nnz = nza;
+        id.irn = ia;
+        id.jcn = ja;
+        id.a = a;
+        id.rhs = b;
+    }
 
 	id.n = n ;	/* number of rows */
 	id.nnz = nza;	/* number of non-zeroes */
 	id.irn = ia;	/* column coordinate */
 	id.jcn = ja;	/* row coordinate */
 	id.a = a;	/* matrix element */
+
+
 	
 
 
 	id.job = 1;  /* analysis */
-
+    dmumps_c(&id);
 	id.job = 2;	/* factorization */
-
+    dmumps_c(&id);
 	id.job = 3; /* compute solution */
-
+    dmumps_c(&id);
 
 
 	id.job = -2; /* terminate (deallocate) job */
-
+    ierr = MPI_Finalize();
 	return 0;
 }
--- src/interfaces/mumps/test_mumps.c
+++ src/interfaces/mumps/test_mumps.c
@@ -21,21 +21,24 @@
 
 
 int main (int argc, char **argv){
-	int n=5;
-	int nnz = 12;
-	int irn[] = {1, 2, 4, 5, 2, 1, 5, 3, 2, 3, 1, 3};
-	int jcn[] = {2, 3, 3, 5, 1, 1, 2, 4, 5, 2, 3, 3};
-	double a[] = {3.0, -3.0, 2.0, 1.0, 3.0, 2.0, 4.0, 2.0, 6.0, -1.0, 4.0, 1.0};
-	double b[] = {20.0, 24.0, 9.0, 6.0, 13.0};
+	int i;
+    int n = 3; /*! something interesting here */
+	int nnz = 6;
+	int irn[] = {1, 1, 1, 2, 2, 3};
+	int jcn[] = {1, 2, 3, 2, 3, 3};
+	double a[] = {5.0, 6.0, 7.0, 3.0, 2.0, 1.0};
+	double b[] = {20.0, 24.0, 9.0};
 	int n_rhs = 1;
 	double *x=NULL;
-	int nvar=5, ncon=5;
+	int nvar=3, ncon=3;
 	int no_inertia=1;
 	double logmu0 = -8.99;
 	int retval;
 
 	retval = mumps_driver(irn, jcn, a, n, n_rhs, b, x, nvar, ncon, no_inertia, nnz, logmu0);
-
+    for(i=0; i<3; i++){
+        printf("result %8.2f\n", *(b + i));
+    }
 	if(retval==0){
         printf("Success\n");
 	}
